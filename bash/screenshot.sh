7f6d9cd1cad1a0fde297acfa642552181753a52c
avudn <avudnworld@gmail.com>
2026-09-19 22:45:24 +0200

    Update

---
+++ bash/screenshot.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+dir="$HOME/Pictures/Screenshots"
+geom=$(slurp) || exit 0 # exit quietly if you cancel the selection
+grim -g "$geom" - | doas tee "$dir/$(date +%Y-%m-%d_%H-%M-%S).png" | wl-copy -t image/png
